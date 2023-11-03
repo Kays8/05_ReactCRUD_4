46470309529bb6bd3dc296f35f70be809f4f8407
Kays8 <thirayut1379@gmail.com>
2023-11-03 21:49:13 +0700

    NewUpdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import { AddStudent, EditStudent, StudentDetail, StudentList } from "./pages";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <>
-  <div className="container">
-    <h1>React.js CRUD Operation</h1>
-  <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<StudentList></StudentList>}></Route>
-    <Route path="/student/create" element={<AddStudent/>}></Route>
-    <Route path="/student/edit/:id" element={<EditStudent/>}></Route>
-    <Route path="/student/detail/:id" element={<StudentDetail/>}></Route>
-    
-  </Routes>
-  </BrowserRouter>
-  </div>
-  </>;
+  return (
+    <>
+      <div className="container">
+        <h1>React.js CRUD Operation</h1>
+        <h2>005 Thirayut Boonin</h2>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<StudentList></StudentList>}></Route>
+            <Route path="/student/create" element={<AddStudent />}></Route>
+            <Route path="/student/edit/:id" element={<EditStudent />}></Route>
+            <Route
+              path="/student/detail/:id"
+              element={<StudentDetail />}
+            ></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
+  );
 }
 
 export default App;
